7fb4fa29bf8a48da28220388de643040a29bc4c1
Geoff Shannon <geoffpshannon@gmail.com>
2014-05-11 17:52:56 -0700

    Add groovy customizations to some auto alists
    auto-mode-alist and interpreter-mode-alist

--- personal/settings.el
+++ personal/settings.el
@@ -12,9 +12,14 @@
 (dolist (auto-mode-pair '(("\\.php\\'" . php-mode)
                           ("\\.rkt\\'" . scheme-mode)
                           ("\\.md\\'" . markdown-mode)
-                          ("\\.markdown\\'" . markdown-mode)))
+                          ("\\.markdown\\'" . markdown-mode)
+                          ("\.groovy$" . groovy-mode)))
+
   (add-to-list 'auto-mode-alist
                auto-mode-pair))
+
+(dolist (auto-interpreter-pair '(("groovy" . groovy-mode)))
+  (add-to-list 'interpreter-mode-alist auto-interpreter-pair))
 
 (setq completion-ignored-extensions
       (append '(".ali" ".exe" ".beam")
