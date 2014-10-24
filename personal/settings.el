3f2320ff1341f1f9e5d5c5f13af5bb5c2669b7c4
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-24 14:21:07 -0400

    Auto-ghost my nick when logging in

--- personal/settings.el
+++ personal/settings.el
@@ -83,6 +83,21 @@
       '(("freenode.net" "#clojure" "#emacs"
          "#wwu-alumni" "#wwucs" "#wwu-devops")))
 
+(defun erc-ghost-maybe (server nick)
+  "Send GHOST message to NickServ if NICK ends with `erc-nick-uniquifier'.
+The function is suitable for `erc-after-connect'."
+  (when (string-match (format "\\(.*?\\)%s+$" erc-nick-uniquifier) nick)
+    (let ((nick-orig (match-string 1 nick))
+          (password erc-session-password))
+      (when (y-or-n-p (format "Current nick is '%s'. Do you want to ghost?"
+                              nick))
+        (erc-message "PRIVMSG" (format "NickServ GHOST %s %s"
+                                       nick-orig password))
+        (erc-cmd-NICK nick-orig)
+        (erc-message "PRIVMSG" (format "NickServ identify %s %s"
+                                       nick-orig password))))))
+(add-hook 'erc-after-connect 'erc-ghost-maybe)
+
 ;; Packages and Stuff
 (prelude-require-packages '(;; Great utilities
                             color-theme
