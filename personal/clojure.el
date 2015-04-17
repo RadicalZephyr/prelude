a4502186ea12d5affb34390e3f941166d4cafddd
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-17 10:39:56 -0500

    Add some personal setup for speclj tests

---
+++ personal/clojure.el
@@ -0,0 +1,13 @@
+(defun speclj-test-ns-fn (ns)
+  "For a NS, return the test namespace, which may be the argument itself.
+This uses the Leiningen convention of appending '-test' to the namespace name."
+  (when ns
+    (let ((suffix "-spec"))
+      ;; string-suffix-p is only available in Emacs 24.4+
+      (if (string-match (rx-to-string `(: ,suffix eos) t) ns)
+          ns
+        (concat ns suffix)))))
+
+(defun speclj-activate-speclj-dev ()
+  (interactive)
+  (setq cider-test-infer-test-ns 'speclj-test-ns-fn))
