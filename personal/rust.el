1c243ca9f1a1f344e34fd42c91f8fc2ded76e904
Geoff Shannon <geoffpshannon@gmail.com>
2016-01-01 14:31:29 -0600

    Enhance my rust config
    - Make compile use the correct command in Cargo.toml files
    - Setup rustfmt to actually run on save

--- personal/rust.el
+++ personal/rust.el
@@ -4,17 +4,23 @@
 
 ;;; Code:
 
+(prelude-require-packages '(rust-mode rustfmt racer company-racer toml-mode))
+
 (require 'compile)
 (require 'rust-mode)
-(add-hook 'rust-mode-hook
-          (lambda ()
-            (set (make-local-variable 'compile-command)
-                 (if (locate-dominating-file (buffer-file-name) "Cargo.toml")
-                     "cargo build"
-                   "rustc *.rs"))))
+(require 'toml-mode)
 
-(prelude-require-packages '(rust-mode rustfmt racer company-racer))
+(defun radz-set-rust-build-command ()
+  (set (make-local-variable 'compile-command)
+       (if (locate-dominating-file (buffer-file-name) "Cargo.toml")
+           "cargo build"
+         "rustc *.rs")))
+
+(add-hook 'rust-mode-hook 'radz-set-rust-build-command)
+(add-hook 'toml-mode-hook 'radz-set-rust-build-command)
 
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
+
+(add-hook 'rust-mode-hook #'rustfmt-enable-on-save)
 ;;; rust.el ends here
