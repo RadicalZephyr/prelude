ef3423cde2bd2bbb106610c1608460047f3dcf61
Geoff Shannon <geoffpshannon@gmail.com>
2017-01-22 15:36:51 -0600

    Split settings.el into more targeted configuration files

---
+++ personal/modes.el
@@ -0,0 +1,19 @@
+(dolist (auto-mode-pair '(("\\.php\\'" . php-mode)
+                          ("\\.rkt\\'" . scheme-mode)
+                          ("\\.md\\'" . markdown-mode)
+                          ("\\.markdown\\'" . markdown-mode)
+                          ("\.groovy$" . groovy-mode)
+                          ("\.gradle$" . groovy-mode)
+                          ("\\.xtm$" . extempore-mode)
+                          ("\\.boot$" . clojure-mode)
+                          ("\\.gpr$" . ada-mode)
+                          ("\\.mustache$" . web-mode)))
+  (add-to-list 'auto-mode-alist
+               auto-mode-pair))
+
+(dolist (auto-interpreter-pair '(("groovy" . groovy-mode)))
+  (add-to-list 'interpreter-mode-alist auto-interpreter-pair))
+
+(setq completion-ignored-extensions
+      (append '(".ali" ".exe" ".beam" ".class")
+              completion-ignored-extensions))
---
+++ personal/windows.el
@@ -0,0 +1,3 @@
+;; Windows specific changes
+(when (eq system-type 'windows-nt)
+  (setq magit-git-executable "C:\\Program Files (x86)\\Git\\bin\\git.exe"))
