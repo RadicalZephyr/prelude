acd6387749ddf7e4fec33ad6d454e8a86648a8e0
Geoff Shannon <geoffpshannon@gmail.com>
2017-09-05 15:04:49 -0500

    Deduplicate rust setups between prelude module and my personal configs

--- personal/rust.el
+++ personal/rust.el
@@ -28,9 +28,6 @@
 (add-hook 'rust-mode-hook 'radz-set-rust-build-command)
 (add-hook 'toml-mode-hook 'radz-set-rust-build-command)
 
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook #'company-mode)
 
-(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 ;;; rust.el ends here
