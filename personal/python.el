1a18d224714b77445e3e01aaa248e8e78e4e2d85
Geoff Shannon <geoffpshannon@gmail.com>
2018-02-13 12:04:03 -0800

    Add some personal python keybindings

---
+++ personal/python.el
@@ -0,0 +1,12 @@
+(require 'pytest)
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c C-a") 'pytest-all)
+            ;; (local-set-key "\C-cm" 'pytest-module)
+            ;; (local-set-key "\C-c." 'pytest-one)
+            ;; (local-set-key "\C-cd" 'pytest-directory)
+            ;; (local-set-key "\C-cpa" 'pytest-pdb-all)
+            ;; (local-set-key "\C-cpm" 'pytest-pdb-module)
+            ;; (local-set-key "\C-cp." 'pytest-pdb-one)
+            ))
