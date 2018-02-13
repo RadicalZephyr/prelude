c8b76a246f62770787ac7eddfdbb3ae5815cf2ad
Geoff Shannon <geoffpshannon@gmail.com>
2018-02-13 12:03:33 -0800

    Add configuration for running format with nightly via rustup

--- personal/rust.el
+++ personal/rust.el
@@ -15,7 +15,8 @@
 
 (setq racer-cmd (concat home-dir "/.cargo/bin/racer")
       racer-rust-src-path (concat home-dir "/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src")
-      rust-format-on-save t)
+      rust-format-on-save t
+      rust-rustfmt-bin "rustup run nightly fmt")
 
 (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
 
