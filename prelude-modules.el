67451ecf673b5eb1bee1267d4c687d95b64df104
Geoff Shannon <geoffpshannon@gmail.com>
2018-03-06 15:04:56 -0800

    Comment out scheme configs

--- prelude-modules.el
+++ prelude-modules.el
@@ -31,7 +31,7 @@
 (require 'prelude-ruby)
 (require 'prelude-rust)
 ;; (require 'prelude-scala)
-(require 'prelude-scheme)
+;; (require 'prelude-scheme)
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
 (require 'prelude-web) ;; Emacs mode for web templates
