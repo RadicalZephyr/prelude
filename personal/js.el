9eaf19c0c8f6e67fd587696f8b92fec01d099fb3
Geoff Shannon <geoffpshannon@gmail.com>
2018-05-26 17:55:05 -0700

    Set js offset to 2 for json mode

--- personal/js.el
+++ personal/js.el
@@ -1,7 +1,11 @@
-(add-hook 'js2-mode-hook (lambda ()
-                           (setq c-basic-offset 2
-                                 js-indent-level 2
-                                 js2-basic-offset 2)))
+(defun radz-set-js-offset ()
+    "Set js offset to 2 probably"
+  (setq c-basic-offset 2
+        js-indent-level 2
+        js2-basic-offset 2))
+
+(add-hook 'js2-mode-hook 'radz-set-js-offset)
+(add-hook 'json-mode-hook 'radz-set-js-offset)
 
 (prelude-require-package 'jasminejs-mode)
 
