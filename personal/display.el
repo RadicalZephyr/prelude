1d87d433e8c15a862b7623dfb34eff31899b5e5d
Geoff Shannon <geoffpshannon@gmail.com>
2018-06-03 03:39:14 -0700

    Organize display settings

--- personal/display.el
+++ personal/display.el
@@ -7,18 +7,22 @@
 ;; (set-face-attribute 'default nil :height 50)
 
 ;; Visual Modifications
-(blink-cursor-mode 1)
-(setq default-tab-width 2)
-(setq x-stretch-cursor t)
-(setq visible-bell t)
 (ansi-color-for-comint-mode-on)
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+(blink-cursor-mode 1)
+(setq
+ default-tab-width           2
+ uniquify-buffer-name-style 'post-forward-angle-brackets
+ visible-bell                t
+ x-stretch-cursor            t
+ )
 
 ;; Scrolling settings
 
-(setq scroll-margin 5
-      scroll-conservatively 10
-      scroll-preserve-screen-position nil)
+(setq
+ scroll-conservatively           10
+ scroll-margin                   5
+ scroll-preserve-screen-position nil
+ )
 
 ;; Enable some modes
 (dolist (mode '(column-number-mode
