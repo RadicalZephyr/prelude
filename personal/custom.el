5b2975765d6f7210ac31e40e477b42ea3437f935
Geoff Shannon <geoffpshannon@gmail.com>
2018-09-21 23:05:33 -0700

    Cider jack-in customs

--- personal/custom.el
+++ personal/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cider-jack-in-auto-inject-clojure "1.8.0")
  '(company-quickhelp-color-background "#4F4F4F")
  '(company-quickhelp-color-foreground "#DCDCCC")
  '(custom-safe-themes
