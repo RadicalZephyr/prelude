ddac461bae23fe2df8a026705c66417911fc3147
Zephyr Shannon <earthlingzephyr@gmail.com>
2020-09-12 03:02:04 -0700

    Correct binding for rg-menu

--- personal/keys.el
+++ personal/keys.el
@@ -17,7 +17,7 @@
 (global-set-key (kbd "C-c c b") 'ibuffer)
 (global-set-key (kbd "C-c c g") 'find-grep-dired)
 (global-set-key (kbd "C-c c o") 'occur)
-(global-set-key (kbd "C-c c s") #'rg-menu)
+(global-set-key (kbd "C-c c s") 'rg-menu)
 (global-set-key (kbd "C-c c l") #'sort-lines)
 
 (global-set-key (kbd "C-c m l") #'mc/edit-lines)
